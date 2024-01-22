913ae378d38de525847206678dc8764104cd0534
LiamQuinlan <122911760+LiamQuinlan@users.noreply.github.com>
2024-01-22 14:09:58 -0500

    Add files via upload
    Copy & pasted header file from my Linked List to set.h. Changed all "list" to set in set.h.

--- set.h
+++ set.h
@@ -1,1 +1,55 @@
+//========================================================
+// Liam Quinlan, Andrew Angulo
+// January 2024
+// set.h
+// This file contains the Set class declaration.  
+//========================================================
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+#ifndef SET_H
+#define SET_H
+
+#define DEFAULT_SET_CAPACITY 10
+
+template <class T> 
+class Set
+{
+public:
+			Set		( void );
+			Set		( const Set<T> &myset );
+		   ~Set		( void );
+Set<T>		operator=	( const Set<T> &myset );
+string		to_string	( void ) const;	
+void		append		( const T &item	);
+T &			operator[]	( int index );
+void		insert		( const T &item, int index );
+void		remove		( int index );
+Set<T>		operator+	( const Set<T> &myset ) const;
+int			length		( void ) const;
+bool		isEmpty		( void ) const;
+void		clear		( void );
+
+friend ostream & operator<< ( ostream &os, Set<T> &myset )
+{
+	for ( int i = 0; i < myset.size; i++ )
+		os << myset.set[i] << " ";
+	return os;	
+}
+
+private:
+	T	*set;			// the pointer for the dynamic array
+	int capacity;		// the size of the array
+	int size;			// the number of items in the set
+
+	void	reallocate	( void );
+};
+
+
+#include "Set.cpp"
+
+#endif
+
 
