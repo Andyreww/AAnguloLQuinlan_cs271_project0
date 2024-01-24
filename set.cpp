5403b7107ea55e5a5fb8e08df24e356552e3d20a
Andrew Angulo <angulo_a1@denison.edu>
2024-01-23 23:58:31 -0500

    Cleaned Code & Added functions

--- set.cpp
+++ set.cpp
@@ -1,1 +1,226 @@
+//=================================================
+// Liam Quinlan, Andrew Angulo
+// January 2024
+// set.cpp
+// This file contains the class implementation for the 
+// Set class.
+//=================================================
 
+#include "set.h"
+#include <iomanip>
+#include <iostream>
+
+/**
+ * @brief Default constructor for Set.
+ *
+ * Creates an empty Set object with capacity 10.
+ *
+ * @note No parameters are required for this constructor.
+ *
+ * @returns None.
+ */
+ template <class T> 
+ Set<T>::Set(void){
+    Set = new T[DEFAULT_SET_CAPACITY]; //Set Size is 10
+    capacity = DEFAULT_SET_CAPACITY;
+    size = 0;
+}
+
+/**
+ * @brief Destructor for Set.
+ *
+ * Completely deletes the Set for existence.
+ *
+ * @note No parameters are required.
+ *
+ * @returns None.
+ */
+template <class T>
+Set<T>::~Set(void){
+    delete []Set;
+}
+
+/**
+ * @brief Set --> toString
+ *
+ * converts the Set into a readable string
+ * ex: {1, 2, 3, 4}
+ *
+ * @note No parameters are required.
+ *
+ * @returns string of the set.
+ */
+template <class T>
+string Set<T>::toString(void) const{
+    stringstream stream;
+
+    for(int i = 0; i < size; i++){
+        stream << Set[i] << ", ";
+    }
+
+    return "{" + stream.str() + "}";
+}
+
+/**
+ * @brief appends values
+ *
+ * appends item of type T to the Set
+ *
+ * @param T item to be appended to the Set
+ *
+ * @returns none.
+ */
+template <class T>
+void Set<T>::append(constant T &item){
+    if(size == capacity) reallocate();
+    Set[size] = item;
+    size++;
+}
+
+/**
+ * @brief reallocates set
+ *
+ * creates new Set object of twice the capacity
+ *
+ * @note No parameters are required.
+ *
+ * @returns none.
+ */
+template <class T>
+void Set<T>::reallocate(void){
+    capacity *= 2;
+
+    T* newptr;
+    newptr = new T[capacity];
+
+    for(int i = 0; i<size; i++) newptr[i] = Set[i];
+
+    delete Set;
+
+    Set = newptr;
+}
+
+/**
+ * @brief Empty
+ *
+ * returns T/F if the Set is empty or not
+ *
+ * @note No parameters are required.
+ *
+ * @returns boolean.
+ */
+template <class T>
+bool Set<T>::empty(void) const{
+    return (size == 0);
+}
+
+/**
+ * @brief Cardinality
+ *
+ * returns the length of a Set
+ *
+ * @note No parameters are required.
+ *
+ * @returns int.
+ */
+template <class T>
+bool Set<T>::cardinality(void) const{
+    return size;
+}
+
+/**
+ * @brief clear
+ *
+ * Removes all items from the Set object
+ *
+ * @note No paramters are required.
+ *
+ * @returns none
+ */
+template <class T>
+void Set<T>::clear(void){
+    size = 0;
+    T* newptr;
+    newptr = new T[capacity];
+
+    delete []Set;
+    Set = newptr;
+}
+
+/**
+ * @brief operator=
+ *
+ * creates new Set object which like terms
+ *
+ * @param mySet a Set object
+ *
+ * @returns Set object.
+ */
+template <class T>
+Set<T> Set<T>::operator=(const Set<T> &mySet){
+    delete []Set;
+
+    size = mySet.size;
+    capacity = mySet.capacity;
+    Set = new T[mySet.capacity];
+
+    for(int i = 0; i < size; i++) Set[i] = mySet.Set[i];
+
+    return *this;
+}
+
+/**
+ * @brief operator+
+ *
+ * adds 2 sets together, doesn't contain duplicates, Plus = Union
+ *
+ * @param mySet a Set object
+ *
+ * @returns Set object.
+ */
+template <class T>
+Set<T> Set<T>::operator+(const Set<T> &mySet){
+    Set<T> newSet;
+    newSet.Set = new T[capacity + mySet.capacity];
+    newSet.capacity = capacity + mySet.capacity;
+
+    // copying elements from the first Set
+    for(int i = 0; i < size; i++) newSet.Set[i] = Set[i];
+
+    newSet.size = size;
+
+    // copying the second Set but checking for dupes
+    for(int i = 0; i < mySet.size; i++)
+    {
+        bool isDupe = false;
+        for(int j = 0; j < newSet.size; j++)
+        {
+            if(newSet.Set[j] == mySet.Set[i])
+            {
+                isDupe = true;
+                break;
+            }
+        }
+        if(!isDupe)
+        {
+            newSet.Set[newSet.size] = mySet.Set[i];
+            newSet.size++;
+        }
+    }
+    return newSet;
+        
+}
+
+/**
+ * @brief operator<=
+ *
+ * Logically idk
+ *
+ * @param mySet a Set object
+ *
+ * @returns boolean.
+ */
+template <class T>
+bool Set<T>::operator<=(const Set<T> &mySet){
+    // im stuck here
+}
--- set.h
+++ set.h
@@ -15,36 +15,38 @@ using namespace std;
 #define DEFAULT_SET_CAPACITY 10
 
 template <class T> 
-class Set
-{
-public:												// (need to create "contains", <= operator, & operator, and - operator)
-			Set		( void );							// KEEP
-			Set		( const Set<T> &myset );				// KEEP
-		   ~Set		( void );							// KEEP
-Set<T>		operator=	( const Set<T> &myset );				// KEEP (need to create s.contains(x) and s <= t from this???)
-string		to_string	( void ) const;					// KEEP
-void		append		( const T &item	);
-T &			operator[]	( int index );
-void		insert		( const T &item, int index );			// KEEP
-void		remove		( int index );						// KEEP
-Set<T>		operator+	( const Set<T> &myset ) const;		// KEEP
-int			length		( void ) const;				// KEEP rename to s.cardinality()
-bool		isEmpty		( void ) const;					// KEEP rename to s.empty()
-void		clear		( void );
+class Set{
+public:													// (need to create "contains", <= operator, & operator, and - operator)
+	Set(void);											// KEEP (creates Set Object)
+	Set(const Set<T> &myset);							// KEEP (copies the set)
+	~Set(void);											// KEEP (Deletes the set)
+	Set<T> operator=(const Set<T> &myset);				// KEEP  
+	string toString(void) const;						// KEEP (returns set as a string {1, 2, 3})
+	void append	(const T &item);						// KEEP (adds value into the set)
+	T &	operator[](int index);
+	void insert(const T &item, int index);				// KEEP (inserts element into index)
+	void remove(int index);								// KEEP (removes element from set)
+	Set<T> operator+(const Set<T> &myset) const;		// KEEP (Adds 2 sets together) !NO DUPLICATES
+	int	cardinality(void) const;						// KEEP (returns the length of the set)
+	bool empty(void) const;								// KEEP (returns T/F if set is empty)
+	void clear(void);									// KEEP (removes all values in set)
+	bool contains(void) const;							// KEEP (returns T/F if its in set)
 
-friend ostream & operator<< ( ostream &os, Set<T> &myset )
-{
-	for ( int i = 0; i < myset.size; i++ )
-		os << myset.set[i] << " ";
-	return os;	
-}
+	bool operator<=(const Set<T> &myset) const;         // How would this work logically --> s <= t from this???)
+
+	friend ostream & operator<< ( ostream &os, Set<T> &myset )
+	{
+		for ( int i = 0; i < myset.size; i++ )
+			os << myset.set[i] << " ";
+		return os;	
+	}
 
 private:
-	T	*set;			// the pointer for the dynamic array
-	int capacity;		// the size of the array
+	T	*Set;			// the pointer for the dynamic set
+	int capacity;		// the size of the set
 	int size;			// the number of items in the set
 
-	void	reallocate	( void );
+	void reallocate(void);
 };
 
 
