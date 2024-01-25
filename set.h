8ea35ce4429177e1f20fce22877c2fe54f63e38d
Andyreww <98863478+Andyreww@users.noreply.github.com>
2024-01-24 22:09:19 -0500

    modified set.h

--- set.h
+++ set.h
@@ -5,62 +5,50 @@
 // This file contains the Set class declaration.  
 //========================================================
 
-#include <cstdlib>
 #include <iostream>
 #include <string>
 using namespace std;
 
-#ifndef SET_H
-#define SET_H
+#ifndef LIST_H
+#define LIST_H
 
-#define DEFAULT_SET_CAPACITY 10
+#define DEFAULT_LIST_CAPACITY 10
 
 template <class T> 
-class Set{
-public:													// (need to create "contains", <= operator, & operator, and - operator)
-	Set(void);											// KEEP (creates Set Object)
-	Set(const Set<T> &myset);									// KEEP (copies the set)
-	~Set(void);											// KEEP (Deletes the set)
-	Set<T> operator=(const Set<T> &myset);								// KEEP  
-	string toString(void) const;									// KEEP (returns set as a string {1, 2, 3})
-	void append(const T &item);								// KEEP (adds value into the set)
-	T & operator[](int index);							// DELETE
-	void insert(const T &item, int index);						// KEEP (inserts element into index)
-	void remove(int index);										// KEEP (removes element from set)
-	Set<T> operator+(const Set<T> &myset) const;							// KEEP (Adds 2 sets together) !NO DUPLICATES
-	bool cardinality(void) const;									// KEEP (returns the length of the set)
-	bool empty(void) const;										// KEEP (returns T/F if set is empty)
-	void clear(void);										// KEEP (removes all values in set)
-	bool contains(void) const;									// KEEP (returns T/F if its in set)
+class Set
+{
+public:
+			Set		( void );
+			Set		( const Set<T> &mylist );
+		   ~Set		( void );
+Set<T>		operator=	( const Set<T> &mylist );
+string		to_string	( void ) const;	
+void		append		( const T &item	);
+T &			operator[]	( int index );
+void		insert		( const T &item, int index );
+void		remove		( int index );
+Set<T>		operator+	( const Set<T> &mylist ) const;
+int			length		( void ) const;
+bool		isEmpty		( void ) const;
+void		clear		( void );
 
-
-	// NEW FUNCTIONS
-	bool operator<=(const Set<T> &myset) const;         						// How would this work logically --> s <= t from this???)
-	
-	// contains
-	
-	// operator==
-	
-	// operator<=
-	
-	// operator&
-	
-	// operator-
-	
-
-	friend ostream & operator<< ( ostream &os, Set<T> &myset )
-	{
-		for ( int i = 0; i < myset.size; i++ ) os << myset.set[i] << " ";
-		return os;	
-	}
+friend ostream & operator<< ( ostream &os, Set<T> &mylist )
+{
+	for ( int i = 0; i < mylist.size; i++ )
+		os << mylist.list[i] << " ";
+	return os;	
+}
 
 private:
-	T	*Set;			// the pointer for the dynamic set
-	int capacity;			// the size of the set
-	int size;			// the number of items in the set
+	T	*list;			// the pointer for the dynamic array
+	int capacity;		// the size of the array
+	int size;			// the number of items in the list
 
-	void reallocate(void);
+	void	reallocate	( void );
 };
+
+#include "set.cpp"
+
 #endif
 
 
